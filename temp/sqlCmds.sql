dd822312b69b30c5e759089b273583c5a7061a2d
Ekalavya <59222175+blackpantherghost@users.noreply.github.com>
2026-01-11 16:06:36 +0530

    Add files via upload

---
+++ temp/sqlCmds.sql
@@ -0,0 +1,14 @@
+-- SELECT datname FROM pg_database;
+-- CREATE TABLE info(
+-- 	id INT,
+-- 	name VARCHAR(100),
+-- 	city VARCHAR(100)
+-- );
+
+-- INSERT INTO info(id, name, city)
+-- VALUES
+-- (101, 'Raju', 'Delhi'),
+-- (102, 'Sham', 'Hyd'),
+-- (103, 'Amit', 'Mumbai');
+
+SELECT * FROM info;
